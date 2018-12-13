d31e8cad7802b6dad40146afc74f73e8e6de5cc8
Handrews0101 <32401789+Handrews0101@users.noreply.github.com>
2018-12-13 00:20:49 -0500

    Update FXMLExampleController.java

--- FXMLExampleController.java
+++ FXMLExampleController.java
@@ -4,11 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
  
+// Looks at the action and if something happened 
 public class FXMLExampleController {
     @FXML private Text actiontarget;
     
+ // Looks at when the signin button is pressed
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
         actiontarget.setText("Sign in button pressed");
     }
-
 }
